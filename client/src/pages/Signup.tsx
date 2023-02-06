05ebd14532a90052207863af204e9971f165fd73
Ayman Alzedawy <ayman.alzedawy@gmail.com>
2023-02-06 17:42:15 -0500

    Created new HOC for layout of signup and login pages

--- client/src/pages/Signup.tsx
+++ client/src/pages/Signup.tsx
@@ -1,3 +1,26 @@
+import { useRef } from "react";
+import { Button } from "../components/Button";
+import { Input } from "../components/Input";
+
 export function Signup() {
-  return <h1>Signup</h1>;
+  const usernameRef = useRef<HTMLInputElement>(null);
+  const nameRef = useRef<HTMLInputElement>(null);
+  const imageRef = useRef<HTMLInputElement>(null);
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold mb-8 text-center">Signup</h1>
+      <form className="grid grid-cols-[auto,1fr] gap-x-3 gap-y-5 items-center justify-items-end">
+        <label htmlFor="userName">Username</label>
+        <Input id="userName" pattern="\S*" required ref={usernameRef} />
+        <label htmlFor="name">Name</label>
+        <Input id="name" required ref={nameRef} />
+        <label htmlFor="imageUrl">Image Url</label>
+        <Input id="imageUrl" type="url" ref={imageRef} />
+        <Button type="submit" className="col-span-full">
+          Sign Up
+        </Button>
+      </form>
+    </>
+  );
 }
